8176da40937fa136f97ce54f049cea8bac406737
Katuhito Hara <katuhitohara@gmail.com>
2020-09-24 18:07:14 +0900

    ブラウザー経由スクレイピング

---
+++ selenium-capture.py
@@ -0,0 +1,15 @@
+from selenium.webdriver import Firefox, FirefoxOptions
+
+url = "https://www.aozora.gr.jp/cards/000081/files/46268_23911.html"
+
+options = FirefoxOptions()
+options.add_argument('-headless')
+
+browser = Firefox(options=options)
+
+browser.get(url)
+
+browser.save_screenshot("website.png")
+
+browser.quit()
+
