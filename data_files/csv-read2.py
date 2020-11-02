a1213b21821524c09f224bac20e39312e0daf725
Katuhito Hara <rahakatuhito@gmail.com>
2020-11-02 15:09:23 +0900

    DataFiles/yaml/xml/csv/json

---
+++ data_files/csv-read2.py
@@ -0,0 +1,11 @@
+import csv, codecs
+
+# CSVファイルを開く
+filename = "list-sjis.csv"
+fp = codecs.open(filename, "r", "shift_jis")
+
+# 1行ずつ読む
+reader = csv.reader(fp, delimiter=",", quotechar='"')
+for cells in reader:
+    print(cells[1], cells[2])
+    
